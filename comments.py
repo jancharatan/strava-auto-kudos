afd601c0a3db2ed8949e899e36802446bc361dab
Jan Charatan <jan.charatan@gmail.com>
2024-02-19 00:30:35 +0000

    Fix comments.

--- comments.py
+++ comments.py
@@ -8,12 +8,12 @@ comments = [
     "Every day I get up and look through the Forbes list of the richest people in America. If I’m not there, I go to work. — Robert Orben",
     "Whatever you do, always give 100% — unless you’re donating blood. — Bill Murray",
     "Be like a postage stamp; stick to one thing until you get there. — Josh Billings",
-    "Hustle until your haters ask if you’re hiring. — Steve Maraboli"
-    "The difference between try and triumph is just a little umph! — Marvin Phillips"
+    "Hustle until your haters ask if you’re hiring. — Steve Maraboli",
+    "The difference between try and triumph is just a little umph! — Marvin Phillips",
     "Just keep swimming. — Finding Nemo (2002)",
-    "Trying is the first step toward failure. — Homer Simpson"
+    "Trying is the first step toward failure. — Homer Simpson",
     "Never put off till tomorrow what you can do the day after tomorrow. — Mark Twain"
 ]
 
 def fetch_random_comment() -> str:
-    return f"{choice(comments)} {"".join(choices(emojis, 3))}"
+    return f"{choice(comments)} {''.join(choices(emojis, k=3))}"
